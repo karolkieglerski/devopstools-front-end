0e5079c583c9fac9571a09b0a2c7dd3486feb084
Karol Kieglerski <kieglerski.karol@gmail.com>
2017-12-10 19:11:10 +0100

    connect to back-end

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export REACT_APP_USERS_SERVICE_URL=http://localhost:5555
+
+npm start
--- src/App.js
+++ src/App.js
@@ -2,17 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+import UsersList from './components/UsersList';
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      users: []
+    }
+  }
+  componentDidMount() {
+    this.getUsers();
+  }
+  getUsers() {
+    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+    .then((res) => { this.setState({ users: res.data.data.users }); })
+    .catch((err) => { console.log(err); })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">DevOpsTools</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <br/>
+              <h1>All Users</h1>
+              <hr/><br/>
+              <UsersList users={this.state.users}/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
